c8f54b4b0c0191347c39cfc7837cefed84a5dcdd
Kennedy Raine Maranion <kmaranion@gmail.com>
2017-03-21 09:36:29 -0500

    Add files via upload
    Newly Revamped UI

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
-// Hamburger
-// $( ".cross" ).hide();
-// $( ".menu" ).hide();
-// $( ".hamburger" ).click(function() {
-// $( ".menu" ).slideToggle( "slow", function() {
-// $( ".hamburger" ).hide();
-// $( ".cross" ).show();
-// });
-// });
-//
-// $( ".cross" ).click(function() {
-// $( ".menu" ).slideToggle( "slow", function() {
-// $( ".cross" ).hide();
-// $( ".hamburger" ).show();
-// });
-// });
+`	
