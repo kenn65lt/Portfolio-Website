b90a2b023906e39b76209a02d4581ade19a77363
Kennedy Raine Maranion <kmaranion@gmail.com>
2017-03-22 12:48:05 -0500

    Add files via upload
    Portfolio website

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ $(function() {
     $(document).on('click', 'a.page-scroll', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset().top -103
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
