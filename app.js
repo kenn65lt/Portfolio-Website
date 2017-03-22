69c5294ca3b4e0e2bb50a54a634cd369e6afbfb6
Kennedy Raine Maranion <kmaranion@gmail.com>
2017-03-22 00:56:38 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-`	
+// Test
+// alert("kenn");
+var affixElement = '.navbar-default';
+var hidePage = '#hideSlide';
+// $(affixElement).affix({
+//   offset: {
+//     // Distance of between element and top page
+//     top: function () {
+//       return (this.top = $(affixElement).offset().top)
+//     }
+//   }
+// });
+
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $(document).on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
